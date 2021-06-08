fad2731dcdbb5324cac75fc5c01bba3b479e47fd
Jamey0621 <you@example.com>
2021-06-08 09:18:41 -0500

    CocktailDB

---
+++ Models/DrinkByIngModel.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CocktailDB.Models
+{
+    public class DrinkByIngModel
+    {
+        public DrinkByIngModel[] drinks { get; set; }
+        public string strDrink { get; set; }
+            public string strDrinkThumb { get; set; }
+            public string idDrink { get; set; }
+    }
+    public class Rootobject
+    {
+   
+
+
+
+
+}
+
+
+}
